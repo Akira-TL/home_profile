f89610c0a52d67db7283d07479e7e0b004a9104d
Akira-TL <2273076505@qq.com>
2025-02-22 23:48:24 +0800

    优化bash脚本逻辑，为python脚本添加debug模式

--- .local/script/process_finished_report.py
+++ .local/script/process_finished_report.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-import smtplib, os, time, argparse
+import smtplib, os, time, argparse, sys
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+from loguru import logger
 
 
 # 邮箱服务器地址
@@ -68,25 +69,44 @@ parser.add_argument(
     default="",
     help="The command.",
 )
+parser.add_argument(
+    "-v",
+    action="store_true",
+    help="Open the debug.",
+)
 parser.add_argument("pid", nargs="?", help="Process ID if not specified with -p")
 args = parser.parse_args()
 
+# 设置logger等级
 
-def autohalt(pid, receivers, message):
+
+if args.v:
+    log_level = "DEBUG"
+else:
+    log_level = "INFO"
+logger.remove()
+logger.add(
+    sys.stderr,
+    level=log_level,
+    format="<g>{time:YY-MM-DD HH:mm:ss}</g> [<lvl>{level}</lvl>] <c><u>{name}</u></c> | {message}",
+)
+
+
+def watch_pid_sent(pid, receivers, message):
     if pid == None:
         send_message(receivers, message)
         return
 
-    print("\033[92mProcess checking started...\033[0m")
+    logger.info("\033[92mProcess checking started...\033[0m")
     while True:
         ps_string = os.popen(f"ps ax | grep {pid}", "r").read()  # 这里的pid是进程号
         ps_strings = ps_string.strip().split("\n")
-        # print(ps_strings)
+        # logger.info(ps_strings)
         if len(ps_strings) <= 3:
             send_message(receivers, message)
             return
         else:
-            # print('Still',len(ps_strings),'Processes, waiting 60s...')
+            # logger.info('Still',len(ps_strings),'Processes, waiting 60s...')
             time.sleep(60)  # 一分钟后检查一次
 
 
@@ -100,12 +120,16 @@ def send_message(receivers, message):
         .strip()
         .split("\n")[0]
     )
-    import socks
-    import socket
+    if proxy_ip == "":
+        logger.debug("\033[91mNot remote collect\033[0m")
+    else:
+        logger.debug(f"\033[92mRemote collect:\033[91m {proxy_ip}\033[0m")
+        import socks
+        import socket
 
-    # 设置代理服务器
-    socks.set_default_proxy(socks.SOCKS5, proxy_ip, 7899)
-    socket.socket = socks.socksocket
+        # 设置代理服务器
+        socks.set_default_proxy(socks.SOCKS5, proxy_ip, 7899)
+        socket.socket = socks.socksocket
     error_cnt = 0
     while error_cnt < 60 * 24:
         try:
@@ -113,20 +137,23 @@ def send_message(receivers, message):
             server = smtplib.SMTP(mail_host, timeout=30)
             server.starttls()
             server.login(mail_user, mail_pass)
-            server.sendmail(sender, receivers, message.as_string())
-            # print(message.as_string())
+            if args.v:
+                logger.debug(f"\n{message.as_string()}")
+            else:
+                logger.info("\033[92mSending email...\033[0m")
+                server.sendmail(sender, receivers, message.as_string())
             server.quit()
-            print("\033[92mEmail send successfully\033[0m")
+            logger.info("\033[92mEmail send successfully\033[0m")
             return
         except Exception as e:
-            print(f"\033[91mFailed to send email: {e}\033[0m")
+            logger.waring(f"\033[91mFailed to send email: {e}\033[0m")
             time.sleep(60)
             error_cnt += 1
 
 
 if __name__ == "__main__":
-    print(
-        "\033[92mProcess ended in "
+    logger.info(
+        "\033[92mProcess ended in \033[93m"
         + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         + "\033[0m"
     )
@@ -152,6 +179,9 @@ if __name__ == "__main__":
                 )
                 + attachment_file.read()
             )
+            logger.debug(
+                f"\033[92mAttachment content :\033[0m\n{fixed_content.decode("utf-8")}"
+            )
             attachment.set_payload(fixed_content)
         with open(args.attachment_path, "wb") as attachment_file:
             attachment_file.write(fixed_content)
@@ -161,7 +191,7 @@ if __name__ == "__main__":
             f"attachment; filename={os.path.basename(args.attachment_path)}",
         )
         message.attach(attachment)
-    autohalt(args.pid, [args.receivers], message)
+    watch_pid_sent(args.pid, [args.receivers], message)
     # 将args.attachment_path重命名，添加最后发送的时间到末尾，并且保留文件后缀
     if args.attachment_path:
         os.rename(
@@ -171,7 +201,7 @@ if __name__ == "__main__":
                 f"_{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}{os.path.splitext(args.attachment_path)[1]}",
             ),
         )
-        print(
+        logger.info(
             "Log file moved to "
             + args.attachment_path.replace(
                 os.path.splitext(args.attachment_path)[1],
